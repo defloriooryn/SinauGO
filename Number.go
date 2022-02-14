12b70b34882d3c4cab0c6517837ef031b1489fa3
Deflorio Arya Nizam <82637114+defloriooryn@users.noreply.github.com>
2022-02-14 07:34:09 +0700

    Update number.go

--- Number.go
+++ Number.go
@@ -9,8 +9,20 @@ func main() {
 }
 
 /*tipe data integer
-tipe	min	max
-int8	-128	127
-int16	-32768	32767
-int32	-2miliar 2 miliar
-int64	-int64>2miliar	int64>2miliar
+tipe		min			max
+int8		-128			127
+int16		-32768			32767
+int32		-2miliar 		2miliar
+int64		-int64>2miliar		int64>2miliar
+ 
+ tipe data uninteger
+ tipe		min			max
+ uint8		0			255
+ uint16		0			65535
+ uint32		0			4294967295
+ uint64		0			18446744073709551615
+ 
+ float
+*/
+
+
