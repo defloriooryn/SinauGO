5e72de412ba20198a164927b0511bd0bd41b84ef
Deflorio Arya Nizam <82637114+defloriooryn@users.noreply.github.com>
2022-02-14 07:25:02 +0700

    update number.go

--- Number.go
+++ Number.go
@@ -7,3 +7,10 @@ func main() {
 	fmt.Println("Dua =", 2)
 	fmt.Println("Tiga koma Lima =", 3.5)
 }
+
+/*tipe data integer
+tipe	min	max
+int8	-128	127
+int16	-32768	32767
+int32	-2miliar 2 miliar
+int64	-int64>2miliar	int64>2miliar
